d4d9f9c5370d2ee2860e1b43ec4932b2b06bce68
aomelly <sasha080aa@gmail.com>
2026-09-22 00:24:22 +0500

    Complete task01. Module 4

--- task01/src/main/java/com/example/task01/Point.java
+++ task01/src/main/java/com/example/task01/Point.java
@@ -1,39 +1,29 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
     int x;
     int y;
 
     public Point(int x, int y) {
-        // TODO: реализовать
-        throw new UnsupportedOperationException("Конструктор не реализован");
+        this.x = x;
+        this.y = y;
     }
 
-    /**
-     * "Вращает" точку относительно начала координат на 180 градусов
-     */
     public void flip() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException("Метод flip не реализован");
+        int oldX = x;
+        x = -y;
+        y = -oldX;
     }
 
-    /**
-     * Считает расстояние от текущей точки до переданной
-     *
-     * @param point вторая точка
-     * @return расстояние между точками
-     */
     public double distance(Point point) {
-        // TODO: реализовать
-        throw new UnsupportedOperationException("Метод distance не реализован");
+        double dx = (double) x - point.x;
+        double dy = (double) y - point.y;
+
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     @Override
     public String toString() {
-        // TODO: реализовать
-        throw new UnsupportedOperationException("Метод toString не реализован");
+        return "(" + x + ", " + y + ")";
     }
 }
